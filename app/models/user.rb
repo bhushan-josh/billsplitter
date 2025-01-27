769320911e50c8f42f293ff69a7c6dbb466d613e
bhushan-josh <bhushan.nagpure@joshsoftware.com>
2025-01-27 21:21:34 +0530

    Implement bcrypt gem for password hashing and improve validation logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   # assosiations
   has_many :groups
   has_many :group_members
@@ -7,8 +9,9 @@ class User < ApplicationRecord
   has_many :settlements
 
   # validation
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[A-Z0-9._+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\z/i }
-  validates :phone, presence: true, uniqueness: true, length: { minimum: 6 }
+
+  validates :first_name, :last_name, presence: true, length: { minimum: 3, maximum: 10 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([A-Za-z0-9._]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,})\z/, message: "is invalid" }
+  validates :phone, presence: true, uniqueness: true, format: { with: /\A\d{10}\z/, message: "is invalid" }
+  validates :password, format: { with: /\A^[([a-z]|[A-Z])0-9_-]{6,20}\z/,  message: "must contain at least six characters" }
 end
